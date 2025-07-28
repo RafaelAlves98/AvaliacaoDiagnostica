e9b63cfda0ce4eced2f4530e1313e259c2335c10
RafaelAlves98 <sevla.rafa98@gmail.com>
2025-07-28 15:26:29 -0300

    Commitando

--- js/jogo.js
+++ js/jogo.js
@@ -2,3 +2,4 @@ const board = document.getElementById("board");
 const quadrado = document.getElementById("quadrado");
 const jogadores = ["X", "O"];
 let jogadorAtual = jogadores[0];
+const mensagemFinal = document
